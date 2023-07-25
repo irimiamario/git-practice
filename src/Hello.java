a50e2d6b1b24e85903670751b6239e8f87fb0bb4
Irimia Mario <marioirimia@gmail.com>
2023-07-25 15:01:03 +0100

    New files

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        String friendName = "John";
+
+        System.out.println("Hello World Again!" + "from: " + friendName);
+
+    }
+
+}
